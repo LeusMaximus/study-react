662652581e67560cc8930bc58b3416effb8be08f
LeusMaximus <leus.maximus@gmail.com>
2016-09-24 01:07:27 +0300

    using states

--- js/app.js
+++ js/app.js
@@ -2,14 +2,17 @@ let my_news = [
   {
     author: 'Sasha Pechkin',
     text: 'On Thursday, the fourth of...',
+    bigText: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.',
   },
   {
     author: 'Just Vasya',
     text: 'I think that $ shoud be cost 35',
+    bigText: 'and euro 42',
   },
   {
     author: 'Guest',
     text: 'Free. Download. The best site - http://localhost:3000',
+    bigText: 'Actually fee, you just need to read the very long license agreement',
   },
 ];
 
@@ -58,16 +61,44 @@ const Article = React.createClass({
   propTypes: {
     data: React.PropTypes.shape({
       author: React.PropTypes.string.isRequired,
-      text: React.PropTypes.string.isRequired
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired,
     })
+  },
+  getInitialState() {
+    return {
+      visible: false
+    };
+  },
+  readmoreClick(e) {
+    e.preventDefault();
+    this.setState({visible: true});
   },
   render() {
     const data = this.props.data;
+    const visible = this.state.visible;
+    const more = visible ?
+      null :
+      <a
+        onClick={this.readmoreClick}
+        href="#"
+        className="news__readmore">
+        More...
+      </a>;
+
+    const bigText = visible ?
+      <p
+        className="news__big-text">
+        {data.bigText}
+      </p> :
+      null;
 
     return (
       <div className="article">
         <p className="news__author">{data.author}:</p>
         <p className="news__text">{data.text}</p>
+        {more}
+        {bigText}
       </div>
     )
   }
@@ -84,6 +115,6 @@ const Comments = React.createClass({
 });
 
 ReactDOM.render(
-  <App data={{name: 'max'}} />,
+  <App />,
   document.getElementById('root')
 );
