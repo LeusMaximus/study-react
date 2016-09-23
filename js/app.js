dda80b7a4331c4334ee3c0401c8ef4e8bf0ed475
LeusMaximus <leus.maximus@gmail.com>
2016-09-23 10:29:12 +0300

    Add another components

--- js/app.js
+++ js/app.js
@@ -3,8 +3,30 @@ const App = React.createClass({
     return (
       <div className="app">
         Hello everybody, I'm the App component!
+        <News />
+        <Comments />
       </div>
-    )
+    );
+  }
+});
+
+const News = React.createClass({
+  render() {
+    return (
+      <div className="news">
+        Unfortunately, there are no news.
+      </div>
+    );
+  }
+});
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comments">
+        So no news - no comments.
+      </div>
+    );
   }
 });
 
