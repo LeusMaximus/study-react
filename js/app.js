432aa299cc30d2b6641c3d8094a8aaf069cb8868
LeusMaximus <leus.maximus@gmail.com>
2016-09-23 19:36:27 +0300

    Add props to News

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
+let my_news = [
+  {
+    author: 'Sasha Pechkin',
+    text: 'On Thursday, the fourth of...',
+  },
+  {
+    author: 'Just Vasya',
+    text: 'I think that $ shoud be cost 35',
+  },
+  {
+    author: 'Guest',
+    text: 'Free. Download. The best site - http://localhost:3000',
+  },
+];
+
 const App = React.createClass({
   render() {
     return (
       <div className="app">
         Hello everybody, I'm the App component!
-        <News />
+        <News data={my_news} />{/* Add some data property*/}
         <Comments />
       </div>
     );
@@ -12,9 +27,21 @@ const App = React.createClass({
 
 const News = React.createClass({
   render() {
+    const data = this.props.data;
+
+    const newsTemplate = data.map((item, index) => (
+        <div key={index}>
+          <p className="news__author">{item.author}:</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      )
+    )
+
+    console.log(newsTemplate)
+
     return (
       <div className="news">
-        Unfortunately, there are no news.
+        {newsTemplate}
       </div>
     );
   }
@@ -31,6 +58,6 @@ const Comments = React.createClass({
 });
 
 ReactDOM.render(
-  <App />,
+  <App data={{name: 'max'}} />,
   document.getElementById('root')
 );
